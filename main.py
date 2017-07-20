b29d4a7b5b4a02b1eb68c09c0d426f027444dcce
David Lu <lugaozhuo110@gmail.com>
2017-07-20 19:17:07 +0800

    update script:

--- main.py
+++ main.py
@@ -42,82 +42,113 @@ def scraptest():
 	s = requests.Session()
 	s.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
 	# payload = urllib.urlencode({'items': 9999, 'page': 1, 'address1': json_data['straddress'], 'city': json_data['strcity'], 'state': json_data['strstate'], 'zip': json_data['strzip']})
-	r = s.get('https://www.justice.gov/eoir/volume-27')#, params=payload)
-	    
-	soup = BeautifulSoup(r.content, 'html.parser')
-	roots = soup.find_all('root')
-	for root in roots:
-		tables = root.find_all('table')
-		for table in tables:
-			print '---------------------------'
-			tbody = table.find('tbody')
-			tr = tbody.find('tr')
-			tds = tr.find_all('td')
+	scrapurls = []
+	scrapurls.append('https://www.justice.gov/eoir/volume-27')
+	scrapurls.append('https://www.justice.gov/eoir/precedent-decisions-volume-26')
+	scrapurls.append('https://www.justice.gov/eoir/precedent-decisions-volume-25')
+	scrapurls.append('https://www.justice.gov/eoir/precedent-decisions-volume-24')
+	scrapurls.append('https://www.justice.gov/eoir/precedent-decisions-volume-23')
+	scrapurls.append('https://www.justice.gov/eoir/precedent-decisions-volume-22')
+	scrapurls.append('https://www.justice.gov/eoir/precedent-decisions-volume-21')
+	scrapurls.append('https://www.justice.gov/eoir/precedent-decisions-volume-20')
+	scrapurls.append('https://www.justice.gov/eoir/precedent-decisions-volume-19')
+	scrapurls.append('https://www.justice.gov/eoir/precedent-decisions-volume-18')
+	scrapurls.append('https://www.justice.gov/eoir/precedent-decisions-volume-17')
+	scrapurls.append('https://www.justice.gov/eoir/precedent-decisions-volume-16')
+	scrapurls.append('https://www.justice.gov/eoir/precedent-decisions-volume-15')
+	scrapurls.append('https://www.justice.gov/eoir/precedent-decisions-volume-14')
+	scrapurls.append('https://www.justice.gov/eoir/precedent-decisions-volume-13')
+	scrapurls.append('https://www.justice.gov/eoir/precedent-decisions-volume-12')
+	scrapurls.append('https://www.justice.gov/eoir/precedent-decisions-volume-11')
+	scrapurls.append('https://www.justice.gov/eoir/precedent-decisions-volume-10')
+	scrapurls.append('https://www.justice.gov/eoir/precedent-decisions-volume-09')
+	scrapurls.append('https://www.justice.gov/eoir/precedent-decisions-volume-08')
 
-			#Name and Date Info
-			p = tds[0].find('p')
-			if p is None:
-				p = tds[0]
-			tmpstrongs = p.find_all('strong')
-			if len(tmpstrongs) == 0:
-				tmpstrongs = p.find_all('b')
-			
-			name = ''
-			for v in tmpstrongs:
-				name = name + ' ' + v.string
-			if name != '':
-				name = utils.removespecialchars(name)
-				#name = re.sub(r'[\xc2]', '', name)
-				name = utils.wholerstrip(name, ' ')
-				name = utils.wholerstrip(name, '-')
-				name = utils.wholerstrip(name, ',')
-				name = utils.wholerstrip(name, ' ')
-
-			dateinfo = ''
-			if len(tmpstrongs) > 0:
-				lastindex = len(tmpstrongs) - 1
-				dateinfo = tmpstrongs[lastindex].nextSibling
-			if dateinfo != '':
-				dateinfo = utils.wholelstrip(dateinfo, ' ')
-				dateinfo = utils.wholelstrip(dateinfo, '-')
-				dateinfo = utils.wholelstrip(dateinfo, ',')
-				dateinfo = utils.wholelstrip(dateinfo, ' ')
-
-			#Download Url
-			pdfurl = ''
-			if len(tds) > 1:
-				ahref = tds[1].find('a')
-				if ahref is not None:
-					pdfurl = ahref.get('href')
-			if 'http' not in pdfurl:
-				pdfurl = 'https://justice.gov' + pdfurl
-
-			# print 'Name:' + name + '|'
-			# print 'Date Info:', dateinfo
-			print 'PDF url:', pdfurl
+	for urlpath in scrapurls:
+		r = s.get(urlpath)#, params=payload)
+		    
+		soup = BeautifulSoup(r.content, 'html.parser')
+		roots = soup.find_all('root')
+		for root in roots:
+			tables = root.find_all('table')
+			for table in tables:
+				print '---------------------------'
+				tbody = table.find('tbody')
+				tr = tbody.find('tr')
+				tds = tr.find_all('td')
 
 
-			#Get Description			
-			nextNode = table
-			description = ''
-			while True:
-				nextNode = nextNode.nextSibling
-				tag_name = ''
-				try:
-					tag_name = nextNode.name
-				except AttributeError:
+				#Name and Date Info
+				name = ''
+				if len(tds) > 0:
+					p = tds[0].find('p')
+					if p is None:
+						p = tds[0]
+					
+					tmpstrongs = p.find_all('strong')
+					if len(tmpstrongs) == 0:
+						tmpstrongs = p.find_all('b')
+					
+					for v in tmpstrongs:
+						try:
+							name = name + ' ' + v.string
+						except Exception, e:
+							pass
+					if name != '':
+						name = utils.removespecialchars(name)
+						#name = re.sub(r'[\xc2]', '', name)
+						name = utils.wholerstrip(name, ' ')
+						name = utils.wholerstrip(name, '-')
+						name = utils.wholerstrip(name, ',')
+						name = utils.wholerstrip(name, ' ')
+
+					dateinfo = ''
+					if len(tmpstrongs) > 0:
+						lastindex = len(tmpstrongs) - 1
+						dateinfo = tmpstrongs[lastindex].nextSibling
+					if dateinfo != '':
+						dateinfo = utils.wholelstrip(dateinfo, ' ')
+						dateinfo = utils.wholelstrip(dateinfo, '-')
+						dateinfo = utils.wholelstrip(dateinfo, ',')
+						dateinfo = utils.wholelstrip(dateinfo, ' ')
+
+				#Download Url
+				pdfurl = ''
+				if len(tds) > 1:
+					ahref = tds[1].find('a')
+					if ahref is not None:
+						pdfurl = ahref.get('href')
+				if 'http' not in pdfurl:
+					pdfurl = 'https://justice.gov' + pdfurl
+
+				print 'Name:' + name + '|'
+				print 'Date Info:', dateinfo
+				print 'PDF url:', pdfurl
+
+
+				#Get Description			
+				nextNode = table
+				description = ''
+				while True:
+					if nextNode is None:
+						break;
+					nextNode = nextNode.nextSibling
 					tag_name = ''
-				if tag_name == 'p':
-					tmpstr = ''.join(nextNode.findAll(text=True))
-					if tmpstr:
-						description = description + ' ' + tmpstr
-				if tag_name == 'hr':
-					break;
-			# print 'Description:', description
+					try:
+						tag_name = nextNode.name
+					except AttributeError:
+						tag_name = ''
+					if tag_name == 'p':
+						tmpstr = ''.join(nextNode.findAll(text=True))
+						if tmpstr:
+							description = description + ' ' + tmpstr
+					if tag_name == 'hr':
+						break;
+				print 'Description:', description
 
-			#Download PDF file
-			filename = downloadFile(pdfurl)
-			dbmodel.addRecord(name, dateinfo, pdfurl, filename)
+				#Download PDF file
+				filename = downloadFile(pdfurl)
+				dbmodel.addRecord(name, dateinfo, pdfurl, filename)
 
 
 if __name__ == '__main__':
